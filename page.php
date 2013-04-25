112a2c3e7c1dbeb488ea1df5a2d28fa0a96aef1a
asimone <andrew.simone@gmail.com>
2013-04-25 13:21:06 -0500

    Create page.php

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+  <article>
+
+<!--[if IE]>
+<div id="ieisbad">Using Internet Explorer promotes bad coding habits, isn't very secure, and makes web designers cry (plus this site is not optimized for it). Please consider getting a more modern browser like <a href="http://www.firefox.com">Firefox</a> or <a href="http://www.google.com/chrome"> Chrome</a>.</div>
+<![endif]-->    
+
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+			<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+			
+			<?php the_content($more_link_text); wp_link_pages();?>
+			<?php endwhile; endif; ?> 
+	
+<?php comments_template( '', true ); ?>
+	</article>
+
+<?php wp_footer(); ?>
