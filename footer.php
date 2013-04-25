24ac6605b69e531da4eb4397f977b0fc2184ef86
asimone <andrew.simone@gmail.com>
2013-04-25 13:19:51 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+   </section>
+    <footer>
+  <section id="footertext">
+		<p id="tagline"><?php bloginfo('description'); ?></p>
+		<p id="designed">Designed by <a href="http://andrewsimone.com">Andrew Simone</a></p>
+    	</section>
+</footer>
+  
+</body>
+</html>
